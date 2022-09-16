ee96ba7a2b9c650a9cdbb86c70b61dfcbce9e174
Vinicius Freitas Pinheiro <viniciusfreitasp@hotmail.com>
2022-09-15 22:14:01 -0300

    working-on-login

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,6 +1,24 @@
+import styles from "./LoginPage.module.scss"
+
 function LoginPage () {
+
     return(
-        <h1>Login</h1>
+        <div className={styles.login}>
+            <h1 className="text__login">Bem Vindo! <br />Realize o login para continuar</h1>
+            <form className={styles.login__form}>
+                <div className={styles.input__container}>
+                    <label>Nome de Usuario</label>
+                    <input type="text" placeholder="Digite aqui..."/>
+                </div>
+                <div className={styles.input__container}>
+                    <label>Senha</label>
+                    <input type="password" placeholder="Digite aqui..."/>
+                </div>
+                <div className={styles.button__login}>
+                    <button type="submit">Entrar</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
